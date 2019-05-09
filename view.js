eee5959271f5eefd2344c3d6fcb8e85bcf988f4d
John Kosmicke <kozjob@yahoo.com>
2019-05-08 18:44:07 -0700

    update

--- view.js
+++ view.js
@@ -1,8 +1,16 @@
 
 
 view2048 = {
+  
+  model: model2048,
+
   init: function(  ) {
-    
+    this.$grid = $("#grid-2048");
+    for( var i = 0; i < this.model.numSquares ; i++ ) {
+      var $square = $("<div><div class='value'>" + this.model.values[i] + "</div></div>");
+      $square.addClass("square");
+      this.$grid.append( $square );
+    }
   },
 };
 
