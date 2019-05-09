2c07527cae1ef1a02a19a1a2d4d441750a021224
John Kosmicke <kozjob@yahoo.com>
2019-05-08 19:08:08 -0700

    update

--- model.js
+++ model.js
@@ -7,6 +7,13 @@ model2048 = {
   side: 4,
   numSquares: 16,
 
+  rowIndexes: {
+    "left": [ [0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15] ],
+    "right": [ [3,2,1,0], [7,6,5,4], [11,10,9,8], [15,14,13,12] ],
+    "up": [ [0,4,8,12], [1,5,9,13], [2,6,10,14], [3,7,11,15] ],
+    "down":[ [12,8,4,0], [13,9,5,1], [14,10,6,2], [15,11,7,3] ],
+  },
+
   values: [],
 
   init: function(  ) {
@@ -24,6 +31,39 @@ model2048 = {
       if( this.values[i] === 0  ) zeroes.push(i);
     }
     var rand = Math.floor(Math.random() * zeroes.length);
-    this.values[rand] = (Math.random() < 0.9 ? 2 : 4);
+    this.values[zeroes[rand]] = (Math.random() < 0.9 ? 2 : 4);
+  },
+
+  move: function( direction ) {
+    this.updateGrid( direction );
+    this.addRandomTile();
+  },
+
+  updateGrid: function( direction ) {
+    var indexes = this.rowIndexes[direction];
+    var that = this;
+    for( var i = 0; i < indexes.length ; i++ ) {
+      var arr = indexes[i];
+      var currentRow = arr.map( function(n){
+        return that.values[n];
+      } );
+      var newRow = this.collapseRow(currentRow);
+      for( var ai = 0; ai < arr.length ; ai++ ) {
+        this.values[ arr[ai] ] = ( newRow[ai] ? newRow[ai] : 0 );
+      }
+    }
+  },
+
+  collapseRow: function( arr ) {
+    var notZero = function(n) { return n !== 0; };
+    var workingArr = arr.filter( notZero );
+    for( var i = 0; i < workingArr.length ; i++ ) {
+      if( workingArr[i] === workingArr[i+1] ){
+        workingArr[i] *= 2;
+        workingArr[i+1] = 0;
+      }
+    }
+    workingArr = workingArr.filter( notZero );
+    return workingArr;
   },
 }
--- view.js
+++ view.js
@@ -4,14 +4,40 @@ view2048 = {
   
   model: model2048,
 
+  colors: [ "papayawhip", "darkkhaki", "coral", "sienna", "dodgerblue", "mediumblue", "orange", "burgundy", "lime", "aquamarine" ],
+
   init: function(  ) {
+    this.update();
+    this.addClickHandler();
+  },
+
+  update: function( args ) {
     this.$grid = $("#grid-2048");
+    this.$grid.empty();
+
     for( var i = 0; i < this.model.numSquares ; i++ ) {
-      var $square = $("<div><div class='value'>" + this.model.values[i] + "</div></div>");
+      var value = this.model.values[i] || "&nbsp";
+      var color = this.colors[0];
+      if( value > 0 ) color = this.colors[ Math.log2(value) ];
+      var $square = $("<div><div class='value'>" + value + "</div></div>");
+      $square.css("background-color", color);
       $square.addClass("square");
       this.$grid.append( $square );
     }
+    
   },
+
+  addClickHandler: function(  ) {
+    var that = this;
+    window.addEventListener("keydown", function(e){
+      if( e.keyCode === 37 ) that.model.move("left");
+      if( e.keyCode === 38 ) that.model.move("up");
+      if( e.keyCode === 39 ) that.model.move("right");
+      if( e.keyCode === 40 ) that.model.move("down");
+      that.update();
+    })
+  },
+
 };
 
 
